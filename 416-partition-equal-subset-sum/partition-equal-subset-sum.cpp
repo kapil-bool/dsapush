cf53f788951366d223a361c141e0e7db44709326
kapil-bool <kapil122solanki@gmail.com>
2025-04-07 20:24:15 +0530

    Time: 147 ms (29.86%) | Memory: 100.4 MB (38.60%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
+    bool helper(vector<int>& nums,vector<vector<int>>&dp,int index,int target){
+        //base case
+        if(target==0)return true;
+        if(target<0||index<0)return false;
+
+        if(dp[index][target]!=-1)return dp[index][target];
+        //2 opt of command
+       bool include = helper(nums, dp, index - 1, target - nums[index]);
+        bool exclude = helper(nums, dp, index - 1, target);
+
+        return dp[index][target] = (include || exclude);
+
+    }
     bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
+        //tabulation
+        int n=nums.size();
+         int total = accumulate(nums.begin(), nums.end(), 0);
         if (total % 2 != 0) return false;
 
         int target = total / 2;
-        int n = nums.size();
+        // int n = nums.size(); 
+        //create a vector now
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return helper(nums,dp,n-1,target);
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1)); // -1 = unvisited
-        return helper(n - 1, target, nums, dp);
-    }
-
-    bool helper(int index, int target, vector<int>& nums, vector<vector<int>>& dp) {
-        if (target == 0) return true;
-        if (index < 0 || target < 0) return false;
-
-        if (dp[index][target] != -1)
-            return dp[index][target]; // Already solved
-
-        bool include = helper(index - 1, target - nums[index], nums, dp);
-        bool exclude = helper(index - 1, target, nums, dp);
-
-        return dp[index][target] = (include || exclude);
     }
 };

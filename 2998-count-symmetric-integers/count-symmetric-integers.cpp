576c0a2e15f7631b59f85b54f7a31e907bb44e8a
kapil-bool <kapil122solanki@gmail.com>
2025-04-11 22:17:46 +0530

    Time: 63 ms (72.77%) | Memory: 9.8 MB (43.75%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -6,10 +6,21 @@ public:
             //for 1 <= low <= high <= 10^4 means 10000
             //3 wqale to possible henahi hai
             if(i<100 and i%11==0)count++;
-            else if(i<10000 and  1000<=i){
-                int left=(i%100)/10+i%10;
-                int right=(i/1000)+(i%1000)/100;
-                if(left==right)count++;
+            else if (i >= 1000 && i < 10000) {
+                //since limit is mentioned here u can use a for loop till 2 one 
+                std::string str = std::to_string(i);
+                int n=str.length();
+                int half=str.length()/2;
+                int right=0;
+                for(int j=half;j<n;j++){
+                    right+=int(str[j]-'0');
+                }
+                int left=0;
+                for(int j=0;j<half;j++){
+                    left+=int(str[j]-'0');
+                }
+                if(right==left)count++;
+                
             }
         }
        return count;

791681b24f50423d68d25a73b3063a55a2d1895d
kapil-bool <kapil122solanki@gmail.com>
2025-06-16 23:25:07 +0530

    Time: 14 ms (34.93%) | Memory: 12.7 MB (49.61%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-
-        vector<int> dp(n, 1);       // dp[i] = size of largest subset ending at i
-        vector<int> parent(n, -1);  // to reconstruct the path
-        int maxLen = 1, lastIndex = 0;
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
+        sort(nums.begin(),nums.end());
+        vector<int>reconstruct;
+        int n= nums.size();
+        int maxi=1;
+        int lastindex=0;
+        vector<int>dp(n,1),hash(n);
+        //now just fill this 
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int prev=0;prev<i;prev++){
+                //changing condition kya hoti hai for hash that dp[prev]+1 >dp[i]
+               if (nums[i] % nums[prev] == 0 && dp[i] < dp[prev] + 1){
+                    dp[i]=1+dp[prev];
+                    hash[i]=prev; //store the index
                 }
-            }
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                lastIndex = i;
+               if(dp[i]>maxi){
+                maxi=dp[i];
+                lastindex=i;
+               }
             }
         }
+            //now the printing algo
+            vector<int>result; //only hash array and nums array will be needed u push the elemnt nums[index] and 
+            result.push_back(nums[lastindex]);
+            while(hash[lastindex]!=lastindex){
+                lastindex=hash[lastindex];
+                result.push_back(nums[lastindex]);
 
-        // Reconstruct the subset
-        vector<int> result;
-        while (lastIndex != -1) {
-            result.push_back(nums[lastIndex]);
-            lastIndex = parent[lastIndex];
-        }
+            }
+            reverse(result.begin(),result.end());
 
-        reverse(result.begin(), result.end());
         return result;
     }
 };
-

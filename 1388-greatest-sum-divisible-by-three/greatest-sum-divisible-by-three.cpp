da878e5bd9618aae93e787d9bd2404e88fcfcece
kapil-bool <kapil122solanki@gmail.com>
2025-07-05 04:17:27 +0530

    Time: 47 ms (22.78%) | Memory: 46.1 MB (15.23%) - LeetSync

---
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+     int helper(int index,int mod,int n,vector<int>& nums,vector<vector<int>>&dp){
+        //base case u reach the end
+        if(index==n){
+            //so if u get %3=0 for your current choice make it return else return a samller number 
+            return (mod==0)?0:(-1e9);
+        }
+        if(dp[index][mod]!=-1)return dp[index][mod];
+         int take = nums[index] + helper(index + 1, (mod + nums[index]) % 3,n, nums, dp);
+        int not_take=helper(index+1,mod,n,nums,dp);
+
+        return dp[index][mod]=max(take,not_take);
+    }
+    int maxSumDivThree(vector<int>& nums) {
+        //basic take not take /see take all the 3 multiplles for sure 
+        //repeated sub problem exist 
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(3,-1));//3 as of 0 1 2 
+        return helper(0,0,n,nums,dp);
+    }
+};

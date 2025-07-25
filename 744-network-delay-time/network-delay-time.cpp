ea0e3a62005f230b79a6a206197f3db64f003793
kapil-bool <kapil122solanki@gmail.com>
2025-07-25 16:44:35 +0530

    Time: 89 ms (44.04%) | Memory: 44.4 MB (68.23%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        //crete the adj //source is not fixed use the priorty que
+        vector<vector<pair<int,int>>>adj(n+1);
+          for (auto& time : times) {
+            int u = time[0], v = time[1], w = time[2];
+            adj[u].push_back({v, w});
+        }
+        //time and node two parametre
+         vector<int> dist(n + 1, 1e9);
+        dist[k] = 0;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, k}); // {time, node}
+        while(!pq.empty()){
+            auto tpelem=pq.top();
+            pq.pop();
+            int time=tpelem.first;
+            int node=tpelem.second;
+            //if u vitnesh a lower one update it 
+           for (auto neigh : adj[node]) {
+                int next = neigh.first;
+                int weight = neigh.second;
+
+                if (dist[next] > time + weight) {
+                    dist[next] = time + weight;
+                    pq.push({dist[next], next});
+                }
+            }
+        }
+        //if any one is 1e9 return -1 else return themax of time
+        for(int i=1;i<=n;i++){
+            if(dist[i]==1e9) return -1; 
+        }
+
+        return *max_element(dist.begin() + 1, dist.end());
+    }
+};

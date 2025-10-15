b7fbd8beff55b8e2c3c8344262188eecf48297fd
kapil-bool <kapil122solanki@gmail.com>
2025-10-15 15:06:36 +0530

    Time: 231 ms (48.50%) | Memory: 173.8 MB (62.03%) - LeetSync

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        //so if there is just a singlr one then i can do like /2 at end 
+        //but if there is a change chen i have to check that 
+        //switch here 
+        int prevl=0;
+        int ans=0;
+        int n=nums.size();
+        int count=1; //itself will be there
+        for(int i=1;i<n;i++){
+          if(nums[i]>nums[i-1]) count++;
+          else{
+             //this means there is a change 
+             /* 1.store the current count and reset it*/
+             prevl=count;
+             count=1;
+             //now when ever ther is a change in 
+          }
+          ans=max(ans,min(prevl,count));
+          ans=max(ans,count/2);
+        }
+           return ans;
+    }
+};

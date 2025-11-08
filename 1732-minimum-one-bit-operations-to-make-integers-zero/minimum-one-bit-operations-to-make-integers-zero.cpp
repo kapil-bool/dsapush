1a60affd43a1f9df77d73227f1acc55dcbfa13ed
kapil-bool <kapil122solanki@gmail.com>
2025-11-09 01:38:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (55.21%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumOneBitOperations(int n) {
         //grey code one this is 
+        //its more like reverse mapping here
         int ans = 0;
         while (n > 0) {
          ans ^= n;

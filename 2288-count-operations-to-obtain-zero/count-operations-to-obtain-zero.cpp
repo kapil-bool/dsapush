498b4b6c244db4926c88c4270ebd1a8d33301f92
kapil-bool <kapil122solanki@gmail.com>
2025-11-10 01:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (6.36%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        int res = 0;  
-        while (num1 && num2) {
-           
-            res += num1 / num2;
-            num1 %= num2;
-            swap(num1, num2);
+        int count = 0;
+        while(num1!=0 && num2!=0)
+        {
+            count = count +1;
+
+            if (num1 >= num2)
+            {
+                num1 = num1 - num2;
+            }
+            else
+            {
+                num2 = num2 - num1;
+            }
         }
-        return res;
+
+        return count;
     }
 };

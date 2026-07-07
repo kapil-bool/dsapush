9843dc882b8e1c90249298e4143006d4b44da119
kapil-bool <kapil122solanki@gmail.com>
2026-07-08 03:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (42.63%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     long long sumAndMultiply(int n) {
         string s = to_string(n);
-        
         long long x = 0;  
         long long sum = 0;
-        
         for (char c : s) {
             if (c == '0') continue;  
             
